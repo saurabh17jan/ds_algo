edbdd11caad42b79e2ec45fd145af12741ec4292
Saurabh Verma <45552150+saurabh17jan@users.noreply.github.com>
2020-08-13 12:53:28 +0530

    Update 001_two_sum.py

--- interestingQuestion/mySoln/001_two_sum.py
+++ interestingQuestion/mySoln/001_two_sum.py
@@ -1,9 +1,10 @@
 # 1. Two Sum
 # Leet
 # Given nums = [2, 7, 11, 15], target = 9,
-
 # Because nums[0] + nums[1] = 2 + 7 = 9,
 # return [0, 1].
+
+# python3
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
